7b5fe485e49f68e6d4ae8cc8b91e31f390938391
phimes <re0phimes@outlook.com>
2019-05-14 13:34:47 +0800

    change de access database into a general way. as long as the models tablename fits the form.Table. the program would work

--- FlaskApp/blueprints/memoviews.py
+++ FlaskApp/blueprints/memoviews.py
@@ -2,14 +2,8 @@ from flask import Blueprint,render_template, request, flash, get_flashed_message
 from forms import QueryForm,DBForm
 import pandas as pd
 from extensions import db
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from flask import g
-from models import PC_memory
+from models import User, ceshi, memory #这句不能删，用了eval方法了，所以虽然灰的但是删了就报错
 import os
-from flask_sqlalchemy import SQLAlchemy
-
 
 
 content = pd.read_csv("static/aaa.csv", index_col=0)
@@ -19,35 +13,12 @@ headers = content.columns
 count = content.count()[0]
 memoview_bp = Blueprint('memo',__name__)
 
-class User(db.Model):
-    __tablename__ = 'User'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(32))
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
-class ceshi(db.Model):
-    __tablename__ = 'ceshi'
-    aaaa = Column(String(32))
-    bbbb = Column(String(32))
-    ID = Column(Integer, primary_key=True)
-
-    def __init__(self, aaaa, bbbb, ID):
-        self.aaaa = aaaa
-        self.bbbb = bbbb
-        self.ID = ID
 
 
 
 @memoview_bp.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        # pc_memo_data = PC_memory.query.order_by(PC_memory.timestamp.desc())
-        # form = QueryForm()
-        # dbform = DBForm()
         DBType = request.form.get('DBType',type=str,default=None)
         username = request.form.get('username', type=str, default=None)
         password = request.form.get('password', type=str, default=None)
@@ -57,46 +28,12 @@ def index():
         #########
         current_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','{DBType}+mysqlconnector://{username}:{password}@{host}/{DBname}'.format(DBType=DBType,username=username,password=password,host=host,DBname=DBname))
         ##############
-        
-
-#         Base = declarative_base()
-
-#         class User(Base):
-#             __tablename__ = 'User'
-#             id = Column(Integer, primary_key=True)
-#             name = Column(String(32))
-        
-        
-        
-#         class Memory(Base):
-#             __tablename__ = 'memory'
-#             id = Column(Integer, primary_key=True)
-#             timestamp = Column(DateTime)
-#             used = Column(String(32))
-#             free = Column(String(32))
-
-
-#         DB_URI = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}"
-#         g.DB_URI = DB_URI.format(user=username,password=password,host=host,port=3306,db=DBname)
-#         eng = create_engine(DB_URI.format(user=username,password=password,host=host,port=3306,db=DBname))
-
-#         Session = sessionmaker(bind=eng)
-#         session = Session()
-
-        
-
-        if Table == 'ceshi':
-            a = ceshi.query.all()
-            for line in a:
-                print(line)
-                print(type(line))
-            return render_template("tableviews/tableOnlymemory.html", a=a)
-        else:
-            a = User.query.all()
-            print(a)
-#             a = session.query(User).all()
-            return render_template("tableviews/tableOnly.html", a=a)
-
+        tableName = eval(Table)
+        a = tableName.query.all()
+        for line in a:
+            keyDict = line.__dict__.keys()
+            columnNameList = list(keyDict)[1:]
+        return render_template("tableviews/tableOnly.html", a=a, columnNameList=columnNameList)
     else:
         # pc_memo_data = PC_memory.query.order_by(PC_memory.timestamp.desc())
         form = QueryForm()
--- FlaskApp/models.py
+++ FlaskApp/models.py
@@ -2,13 +2,36 @@ from datetime import datetime
 from extensions import db
 
 
-class PC_memory(db.Model):
-    timestamp = db.Column(db.Integer, primary_key=True)
-    available = db.Column(db.Integer)
+class memory(db.Model):
+    __tablename__ = 'memory'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.Integer)
     used = db.Column(db.Integer)
     free = db.Column(db.Integer)
-    percent = db.Column(db.Integer)
+    # free = db.Column(db.Integer)
 
 
 class InRun_memory(db.Model):
     timestamp = db.Column(db.Integer, primary_key=True)
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class ceshi(db.Model):
+    __tablename__ = 'ceshi'
+    aaaa = db.Column(db.String(32))
+    bbbb = db.Column(db.String(32))
+    ID = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, aaaa, bbbb, ID):
+        self.aaaa = aaaa
+        self.bbbb = bbbb
+        self.ID = ID
