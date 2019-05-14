ab7fba73680b4af35cf00eb3cabb24928b449e72
phimes <re0phimes@outlook.com>
2019-05-14 11:26:35 +0800

    use flask-sqlalchemy

--- FlaskApp/blueprints/memoviews.py
+++ FlaskApp/blueprints/memoviews.py
@@ -1,6 +1,7 @@
-from flask import Blueprint,render_template, request, flash, get_flashed_messages, Flask, redirect
+from flask import Blueprint,render_template, request, flash, get_flashed_messages, Flask, redirect, current_app
 from forms import QueryForm,DBForm
 import pandas as pd
+from extensions import db
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -18,6 +19,29 @@ headers = content.columns
 count = content.count()[0]
 memoview_bp = Blueprint('memo',__name__)
 
+class User(db.Model):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32))
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class ceshi(db.Model):
+    __tablename__ = 'ceshi'
+    aaaa = Column(String(32))
+    bbbb = Column(String(32))
+    ID = Column(Integer, primary_key=True)
+
+    def __init__(self, aaaa, bbbb, ID):
+        self.aaaa = aaaa
+        self.bbbb = bbbb
+        self.ID = ID
+
+
+
 @memoview_bp.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
@@ -31,37 +55,46 @@ def index():
         DBname = request.form.get('DBname', type=str, default=None)
         Table = request.form.get('Table', type=str, default=None)
         #########
-#         app = Flask(__name__)
-#         app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','%s://%s:%s@%s/%s' %(DBType,username,password,host,DBname))
-        ######################
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','{DBType}+mysqlconnector://{username}:{password}@{host}/{DBname}'.format(DBType=DBType,username=username,password=password,host=host,DBname=DBname))
+        ##############
+        
 
-        Base = declarative_base()
+#         Base = declarative_base()
 
-        class User(Base):
-            __tablename__ = 'User'
-            id = Column(Integer, primary_key=True)
-            name = Column(String(32))
+#         class User(Base):
+#             __tablename__ = 'User'
+#             id = Column(Integer, primary_key=True)
+#             name = Column(String(32))
+        
+        
+        
+#         class Memory(Base):
+#             __tablename__ = 'memory'
+#             id = Column(Integer, primary_key=True)
+#             timestamp = Column(DateTime)
+#             used = Column(String(32))
+#             free = Column(String(32))
 
 
-        class Memory(Base):
-            __tablename__ = 'memory'
-            id = Column(Integer, primary_key=True)
-            timestamp = Column(DateTime)
-            used = Column(String(32))
-            free = Column(String(32))
+#         DB_URI = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}"
+#         g.DB_URI = DB_URI.format(user=username,password=password,host=host,port=3306,db=DBname)
+#         eng = create_engine(DB_URI.format(user=username,password=password,host=host,port=3306,db=DBname))
 
-        DB_URI = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}"
-        g.DB_URI = DB_URI.format(user=username,password=password,host=host,port=3306,db=DBname)
-        eng = create_engine(DB_URI.format(user=username,password=password,host=host,port=3306,db=DBname))
+#         Session = sessionmaker(bind=eng)
+#         session = Session()
 
-        Session = sessionmaker(bind=eng)
-        session = Session()
-        if Table == 'memory':
-            a = session.query(Memory).all()
-            # return render_template("tableviews/tableOnlymemory.html", a=a)
-            return redirect('/tableviews/queryData/')
+        
+
+        if Table == 'ceshi':
+            a = ceshi.query.all()
+            for line in a:
+                print(line)
+                print(type(line))
+            return render_template("tableviews/tableOnlymemory.html", a=a)
         else:
-            a = session.query(User).all()
+            a = User.query.all()
+            print(a)
+#             a = session.query(User).all()
             return render_template("tableviews/tableOnly.html", a=a)
 
     else:
